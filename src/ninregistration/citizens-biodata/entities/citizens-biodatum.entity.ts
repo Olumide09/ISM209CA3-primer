822230a6fe3867d13d6265c6cfa3ff8679be206a
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-02-24 02:14:13 +0100

    Update citizens-biodatum.entity.ts

--- src/ninregistration/citizens-biodata/entities/citizens-biodatum.entity.ts
+++ src/ninregistration/citizens-biodata/entities/citizens-biodatum.entity.ts
@@ -1,1 +1,35 @@
-export class CitizensBiodatum {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+
+export class CitizensBiodatum {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column({ nullable: true })
+    middleName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ nullable: true })
+    dateOfBirth: Date;
+
+    @Column()
+    countryOfbirth: string;
+
+    @Column()
+    townOfbirth: string;
+
+    @Column()
+    stateOfbirth: string;
+
+    @Column()
+    ResidenceAddress: string;
+
+    @Column()
+    Profession: string;
+
+}
