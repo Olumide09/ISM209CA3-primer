834c99066e77a8db15c538a35e3add62c04f8d71
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-02-24 02:06:43 +0100

    Update citizens-biodata.module.ts

--- src/ninregistration/citizens-biodata/citizens-biodata.module.ts
+++ src/ninregistration/citizens-biodata/citizens-biodata.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CitizensBiodataService } from './citizens-biodata.service';
 import { CitizensBiodataController } from './citizens-biodata.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CitizensBiodatum } from './entities/citizens-biodatum.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [CitizensBiodataController],
   providers: [CitizensBiodataService]
 })
