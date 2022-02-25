74a20159ee27776a51b0cfcef7845df566d2316c
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-02-25 21:51:53 +0100

    Update create-citizens-biodatum.dto.ts

--- src/ninregistration/citizens-biodata/dto/create-citizens-biodatum.dto.ts
+++ src/ninregistration/citizens-biodata/dto/create-citizens-biodatum.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCitizensBiodatumDto {}
+export class CreateCitizensBiodatumDto {
+    readonly firstName: string;
+    readonly middleName?: string;
+    readonly lastName: string;
+    readonly dateOfbirth: Date;
+    readonly Nationality: string;
+    readonly countryOfbirth: string;
+    readonly stateOfbirth: string;
+    readonly townOfbirth: string;
+    readonly ResidenceAddress: string;
+    readonly Profession: string;
+
+}
